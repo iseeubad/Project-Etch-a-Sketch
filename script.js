4bcfe69523d25c28699bbb30c2a1db0f1563d320
iseeubad <mohamedsalimsoulmani2004@gmail.com>
2024-08-28 20:18:52 +0100

    last update

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const container = document.querySelector(".container");
 
-function CreateGrid(size) {
+function createGrid(size) {
     container.innerHTML = ''; 
-    let squareSize = 960/size;
+    let squareSize = 600/size;
 
     for (var i = 0; i<size * size; i++) {
         const cell = document.createElement('div');
         cell.classList.add('grid-cell');
+        cell.style.width = `${squareSize}px`;
+        cell.style.height = `${squareSize}px`;
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = getRandomColor(); 
+        });
         container.appendChild(cell);
     }
 }
@@ -21,3 +26,16 @@ function resizeGrid(size) {
         alert('Please enter a valid number.');
     }
 }
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+sizing.addEventListener('click', resizeGrid);
+
+createGrid(16); // Create initial 16x16 grid
